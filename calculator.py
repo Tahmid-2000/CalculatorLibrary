a80172ee37a22e653275322ecf647dcb10ef91d5
Mohammed Tahmid Hussain <60098030@udst.edu.qa>
2023-11-02 23:12:40 +0300

    Updated calculator.py

--- calculator.py
+++ calculator.py
@@ -1,9 +1,8 @@
-class Calculator:
-    def add(first_term, second_term):
-        return first_term + second_term
+def add(first_term, second_term):
+    return first_term + second_term
 
-    def subtract(first_term, second_term):
-        return first_term - second_term
+def subtract(first_term, second_term):
+    return first_term - second_term
 
-    def multiply(first_term, second_term):
-        return first_term * second_term
+def multiply(first_term, second_term):
+    return first_term * second_term
